b2ec485613b185878cff419070088c53b0c6e22a
frozflame <frozflame@outlook.com>
2021-07-31 20:01:07 +0800

    ver 0.0.2: AsymmetricEncryptionInterface

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 joker-masquerade
 ================
 
-Hide all the pretty names and ugly news until everybody knows it.
-
+Highly experimental security tools.
 
--- joker/masquerade/__init__.py
+++ joker/masquerade/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.0.2!'
+__version__ = '0.0.2'
+
+from joker.masquerade.asymmetric import AsymmetricEncryptionInterface
+
+if __name__ == '__main__':
+    print(__version__)
